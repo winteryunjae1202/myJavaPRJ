cf060d1a157820914369109d079568d5bb2dcb90
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-15 10:24:43 +0900

    Ex03_01

---
+++ src/main/java/Ex03_01.java
@@ -0,0 +1,9 @@
+public class Ex03_01 {
+    public static void main(String[] args) {
+        System.out.printf("100+100");
+        System.out.printf("\n");
+        System.out.printf("%d",100+100);
+        System.out.printf("\n");
+
+    }
+}
