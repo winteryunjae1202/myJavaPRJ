e294340bffa6dc76361f189bff970f8f18313be3
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-23 12:44:18 +0900

    Ex0323

---
+++ src/main/java/Ex0323.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex0323 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("정수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if(a%3 == 0 && a%5 == 0) {
+            System.out.printf("3의 배수와 5의 배수를 둘다 만족합니다.\n");
+        }
+        else if(a%5 == 0) {
+            System.out.printf("5의 배수입니다.\n");
+        }
+        else if(a%3 == 0) {
+            System.out.printf("3의 배수입니다.\n");
+      }
+    }
+}
