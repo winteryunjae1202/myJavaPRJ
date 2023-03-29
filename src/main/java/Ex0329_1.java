e1fa0d7fc47399485487baeba72ded857a23e883
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-29 11:49:01 +0900

    Ex06_14

---
+++ src/main/java/Ex0329_1.java
@@ -0,0 +1,12 @@
+public class Ex0329_1 {
+    public static void main(String[] args) {
+        int i, k;
+
+        for ( i = 1; i < 10; i++){
+            System.out.printf("\n");
+            for ( k = 2; k < 10; k++){
+                System.out.printf("%d * %d = %2d ", k, i, k*i);
+            }
+        }
+    }
+}
