c93afa3296287f54e9f67008758ac1816173f18c
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-30 16:59:49 +0900

    Ex0330

---
+++ src/main/java/Ex0330_1.java
@@ -0,0 +1,15 @@
+public class Ex0330_1 {
+    public static void main(String[] args) {
+        int i, k;
+
+        for (i = 2, k = 1; i < 10; k++) {
+            if (k > 9) {
+                System.out.printf("\n");
+                k = 0;
+                i++;
+            }
+            else System.out.printf("%d x %d = %2d ", i, k, i * k);
+        }
+    }
+}
+
