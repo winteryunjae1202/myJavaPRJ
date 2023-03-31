36eecfdb30a6cf0d9eed406d3385595af9c907e2
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-31 10:28:41 +0900

    Ex0331_1 별 거꾸로 찍기

---
+++ src/main/java/Ex0331_1.java
@@ -0,0 +1,14 @@
+public class Ex0331_1 {
+    public static void main(String[] args) {
+        int i, b;
+
+        for (i = 1, b = 5; i < 6; b--) {
+            System.out.printf("*");
+            if (b <= i) {
+                i++;
+                b = 6;
+                System.out.println("");
+            }
+        }
+    }
+}
