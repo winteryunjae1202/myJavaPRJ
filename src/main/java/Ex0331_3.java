2254c87fcc94f53d0c784ed8b003454acb0a3017
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-31 15:53:27 +0900

    Ex0331_3 별찍기2

---
+++ src/main/java/Ex0331_3.java
@@ -0,0 +1,10 @@
+public class Ex0331_3 {
+    public static void main(String[] args) {
+        int i, j;
+        for( j = 1; j < 6 ; j++ ){
+            for( i = 1; i <= j ; i++){
+                System.out.printf("*");
+            }System.out.println();
+        }
+    }
+}
