0d9c9d17de713be1791d4ac49427886c6e5f4740
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-03-31 11:14:04 +0900

    Ex0331_2 별 삼각형찍기

---
+++ src/main/java/Ex0331_2.java
@@ -0,0 +1,26 @@
+public class Ex0331_2 {
+    public static void main(String[] args) {
+        int i, b;
+
+        for (i = 1, b = 1; i < 6; b++) {
+            System.out.printf("*");
+            if (b >= i) {
+                i++;
+                b = 0;
+                System.out.println("");
+            }
+        }
+        if (i > 5) {
+            for (i = 1, b = 4; i < 5; b--) {
+                System.out.printf("*");
+                if (b <= i) {
+                    i++;
+                    b = 5;
+                    System.out.println("");
+                }
+            }
+        }
+    }
+}
+
+
