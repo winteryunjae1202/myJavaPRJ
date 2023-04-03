eb1cfab76ed928132f76158e954a0af78d293448
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-04-03 17:24:15 +0900

    ScannerS연습3

---
+++ src/main/java/Baekjoon/Baekjoon_18108.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+import java.util.Scanner;
+public class Baekjoon_18108 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int a;
+        a = S.nextInt();
+        System.out.printf("%d", a-543);
+    }
+}
