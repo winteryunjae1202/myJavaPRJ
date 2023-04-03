b6569a8a63e1736cb95146eb5a3386afbce6044d
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-04-03 17:26:27 +0900

    ScannerS연습4

---
+++ src/main/java/Baekjoon/Baekjoon_10869.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+import java.util.Scanner;
+public class Baekjoon_10869 {
+    public static void main(String[]args) {
+        Scanner S = new Scanner(System.in);
+        int a, b;
+        a = S.nextInt();
+        b = S.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
