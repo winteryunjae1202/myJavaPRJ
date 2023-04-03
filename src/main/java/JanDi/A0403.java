80178bd06e1a39e90e39bcfd7d9e84c4cbce0ae2
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-04-03 18:20:45 +0900

    2023.04.03 for문, 문자열출력

---
+++ src/main/java/JanDi/A0403.java
@@ -0,0 +1,24 @@
+package JanDi;
+import java.util.Scanner;
+
+public class A0403 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int a;
+        int b, c;
+        b = S.nextInt();
+        c = S.nextInt();
+        System.out.printf("정수 2개를 입력하세요.\n");
+        for (a = 1; a < 5; a++) {
+            System.out.printf("%d X %d = %d ", a, b, a * b);
+        }
+            System.out.println();
+
+            for (a = 5; a < 10; a++) {
+                System.out.printf("%d + %d = %d ", a, c, a + c);
+                c+=3;
+            }
+        System.out.println();
+
+    }
+}
