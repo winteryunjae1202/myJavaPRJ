a5a9f3048b2606369126a37811466daadc146aa7
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-04-04 17:19:26 +0900

    2023.04.04 백준 11382

---
+++ src/main/java/Baekjoon/Baekjoon_11382.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+import java.util.Scanner;
+public class Baekjoon_11382 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        long a, b, c;
+        a = (long)S.nextInt();
+        b = (long)S.nextInt();
+        c = (long)S.nextInt();
+        System.out.println(a+b+c);
+    }
+}
