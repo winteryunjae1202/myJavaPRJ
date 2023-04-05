291267bc81ae16c7a67355be3192e52e649ee2c0
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-04-05 13:43:51 +0900

    0405 잔디심기 n개의 숫자 평균구하기

---
+++ src/main/java/JanDi/A0405.java
@@ -0,0 +1,22 @@
+package JanDi;
+import java.util.Scanner;
+public class A0405 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.printf("몇개의 숫자를 쓸지 정하세요 : ");
+        int n;
+        int hap = 0, i;
+        float avg;
+        n = s.nextInt();
+        int[]num = new int[n];
+        for(i = 0; i < num.length; i++){
+            System.out.printf("%d번째 숫자를 입력하세요 : ", i+1);
+            num[i] = s.nextInt();}
+        int b = num.length;
+        for(b = 0; b < num.length; b++){
+            hap = hap + num[b];
+        }avg = hap / (float)(n);
+        System.out.printf("평균 ==> %4.1f \n", avg);
+
+    }
+}
