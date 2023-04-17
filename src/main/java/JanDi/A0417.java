ec4188a6094fdcb7a57affd3d869d771e32ecea8
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-04-17 17:34:32 +0900

    0417 잔디심기 등차수열 or 등비수열

---
+++ src/main/java/JanDi/A0417.java
@@ -0,0 +1,22 @@
+package JanDi;
+import java.util.Scanner;
+public class A0417 {
+    public static void main(String[] args) {
+        int n;
+        int result = 0;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = s.nextInt();
+        }
+        if(arr[2] - arr[1] == arr[1] - arr[0]){
+            result = arr[arr.length-1] + (arr[1] - arr[0]);
+        }
+
+        if(arr[2] / arr[1] == arr[1] / arr[0]){
+            result = arr[arr.length-1]*(arr[1]/arr[0]);
+        }
+        System.out.printf("%d", result);
+    }
+}
