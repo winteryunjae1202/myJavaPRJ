ef1e09277b7e64f743c55262cc98a341055ed228
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-05-10 10:12:07 +0900

    예외처리 실습

---
+++ src/main/java/Class/Chapter10/Ex10_04.java
@@ -0,0 +1,16 @@
+package Class.Chapter10;
+
+public class Ex10_04 {
+    public static void main(String[] args) {
+        int a = 100, b = 0;
+        int result;
+        try {
+            if(b == 0)
+                throw new Exception("0으로 나누려고요? 그건 안되는데요.");
+            result = a / b;
+        } catch (Exception e){
+            System.out.print("발생 오류 ==> ");
+            System.out.println(e.getMessage());;
+        }
+    }
+}
