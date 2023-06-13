46d1dcda59e7b28f19e0f920e21c55161918603b
winteryunjae1202 <winteryunjae1202@gmail.com>
2023-06-13 09:51:19 +0900

    0613 잔디심기 코드업 2008번 오름차순 내림차순 판별하기

--- src/main/java/JanDi/A0613.java
+++ src/main/java/JanDi/A0613.java
@@ -27,18 +27,17 @@ public class A0613 {
         int[] sort = Arrays.copyOf(arr, arr.length);     // Arrays.copyOf(배열명, 배열의길이) => 배열명에 들어가는 배열을 새로운 배열을 만들어 저장함.
         Arrays.sort(sort);
         boolean equal1 = Arrays.equals(arr, sort);      // boolean 변수명 = Arrays.equals(배열 1, 배열 2) => 두 배열이 같은 배열인지 여부 확인
-        if(equal1 == true) {
-            System.out.print("오름차순입니다.");
-        }
 
         int[] reverse = Arrays.copyOf(sort, sort.length);
         reverseArray(reverse);
         boolean equal2 = Arrays.equals(arr, reverse);
-        if(equal2 == true){
-            System.out.print("내림차순입니다.");
-        }
-        if(equal1 == false && equal2 == false){
+
+        if((equal1 == false && equal2 == false) || (equal1 == true && equal2 == true)){
             System.out.print("섞임");
+        }else if(equal1 == true) {
+            System.out.print("오름차순입니다.");
+        }else if(equal2 == true){
+            System.out.print("내림차순입니다.");
         }
     }
 }
